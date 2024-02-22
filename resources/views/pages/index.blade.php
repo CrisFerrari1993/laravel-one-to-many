5a968a5e32a03d070467f99df1d2c242c64e53ec
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-02-22 17:34:41 +0100

    Stampa a pagina

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,20 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+    <h1>Users</h1>
+    <ul>
+        @foreach ($users as $user)
+            <li><h2><b>User Name: </b>{{$user->name}}</h2></li>
+            <ul>
+                <h4>Projects:</h4>
+                @foreach ($user->projects as $project)
+                        <li>
+                            title: <h5>{{$project->title}}</h5><br>
+                            framework: {{$project->framework}} <br>
+                            description: {{$project->description}}
+                        </li>
+                @endforeach
+            </ul>
+        @endforeach
+    </ul>
 @endsection
